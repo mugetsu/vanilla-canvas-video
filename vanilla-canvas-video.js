a4c291aeb5671d4f6347cf80ae5b95828c829b89
Randell Quitain <randell.quitain@grey.com>
2017-03-08 12:18:40 +0800

    The video renderer

--- vanilla-canvas-video.js
+++ vanilla-canvas-video.js
@@ -3,18 +3,20 @@
 	"use strict";
 	
 	window.CanvasVideo = function CanvasVideo(element, options) {
-		var _self = this;
+		var self = this;
 		this._settings = {
 			userOptions: {}
 		};
 		if(typeof options !== 'object') return console.error('CanvasVideo only accepts the options as an object.');
 		this._settings.userOptions = options;
 		this.default_options = {
-			container: '',
-			filename: '',
-			autoplay: false,
-			width: '400',
-			height: '400'
+			container: ''
+			, filename: ''
+			, autoplay: false
+			, width: '400'
+			, height: '400'
+			, buffer_id: 'buffer'
+			, output_id: 'output'
 		};
 		this.options = this.setupOptions(options);
 		this.el = element;
@@ -40,10 +42,25 @@
 	};
 
 	CanvasVideoPrototype.renderCanvas = function (options) {
-		var _tpl  = '<canvas width="'+options.width+'" height="'+options.height+'" id="v_buffer"></canvas>';
-			_tpl += '<canvas width="'+options.width+'" height="'+(options.height/2)+'" id="v_output"></canvas>';
+		var _tpl  = '<canvas width="'+options.width+'" height="'+options.height+'" id="'+options.buffer_id+'"></canvas>';
+			_tpl += '<canvas width="'+options.width+'" height="'+(options.height/2)+'" id="'+options.output_id+'"></canvas>';
 		var _target = document.getElementById(options.container);
 		_target.innerHTML = _tpl;
+		this.renderVideo(options);
+	};
+
+	CanvasVideoPrototype.renderVideo = function (options) {
+		var self = this
+			, buffer_canvas = document.getElementById(options.buffer_id)
+			, buffer = buffer_canvas.getContext('2d')
+			, output_canvas = document.getElementById(options.output_id)
+			, output = output_canvas.getContext('2d')
+			, video = document.getElementById('video')
+			, width = output_canvas.width
+			, height = output_canvas.height;
+		var video = document.createElement('video');
+		if(options.autoplay) video.autoplay = 'autoplay';
+		video.src = options.filename;
 	};
 
 })();
