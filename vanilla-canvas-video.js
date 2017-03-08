0114b66092851eede497a28b247f525e4ef6cee3
Randell Quitain <randell.quitain@grey.com>
2017-03-08 12:05:17 +0800

    The options and canvas renderer

--- vanilla-canvas-video.js
+++ vanilla-canvas-video.js
@@ -1,7 +1,40 @@
 (function() {
+	
 	"use strict";
-	window.CanvasVideo = function CanvasVideo() {
+	
+	window.CanvasVideo = function CanvasVideo(element, options) {
 		var _self = this;
+		this._settings = {
+			userOptions: {}
+		};
+		if(typeof options !== 'object') return console.error('CanvasVideo only accepts the options as an object.');
+		this._settings.userOptions = options;
+		this.default_options = {
+			target_element: '',
+			video_file: '',
+			view_width: '300',
+			view_height: '300'
+		};
+		this.options = this.setupOptions(options);
+		this.el = element;
 	};
+
 	var CanvasVideoPrototype = window.CanvasVideo.prototype;
+
+	CanvasVideoPrototype.setupOptions = function(new_options) {
+		var merged_options = {};
+		for (var attrname in this.default_options) {
+			merged_options[attrname] = this.default_options[attrname];
+		}
+		if(this._settings.userOptions) {
+			for (var attrname in this._settings.userOptions) {
+				merged_options[attrname] = this._settings.userOptions[attrname];
+			}
+		}
+		for (var attrname in new_options) {
+			merged_options[attrname] = new_options[attrname];
+		}
+		return merged_options;
+	}
+
 })();
