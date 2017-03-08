796af8b49deff298876fdd16ac446ca3fa04164c
Randell Quitain <randell.quitain@grey.com>
2017-03-08 12:29:19 +0800

    The processor

--- vanilla-canvas-video.js
+++ vanilla-canvas-video.js
@@ -1,6 +1,6 @@
 (function() {
 	
-	"use strict";
+	'use strict';
 	
 	window.CanvasVideo = function CanvasVideo(element, options) {
 		var self = this;
@@ -13,6 +13,8 @@
 			container: ''
 			, filename: ''
 			, autoplay: false
+			, onload: true
+			, looping: false
 			, width: '400'
 			, height: '400'
 			, buffer_id: 'buffer'
@@ -61,6 +63,35 @@
 		var video = document.createElement('video');
 		if(options.autoplay) video.autoplay = 'autoplay';
 		video.src = options.filename;
+		if(options.onload) video.load();
+		video.addEventListener('play', function() {
+			self.processFrames(buffer, output, video, width, height);
+		}, false);
+		if(options.onload) {
+			video.addEventListener('loadeddata', function() {
+				video.play();
+			}, false);
+		}
+		if(options.looping) {
+			video.addEventListener('ended', function() {
+			    video.play();
+			}, false);
+		}
+	};
+
+	CanvasVideoPrototype.processFrames = function(buffer, output, video, width, height) {
+		var self = this;
+		buffer.drawImage(video, 0, 0);
+		var image = buffer.getImageData(0, 0, width, height)
+			, imageData = image.data
+			, alphaData = buffer.getImageData(0, height, width, height).data;
+		for (var i = 3, len = imageData.length; i < len; i = i+4) {
+			imageData[i] = alphaData[i-1];
+		}
+		output.putImageData(image, 0, 0, 0, 0, width, height);
+		setTimeout(function(){
+			self.processFrames(buffer, output, video, width, height);
+		}, 20);
 	};
 
 })();
